12d8e963c89c7d7c87f3d68a3289f7d20dbccf94
Saroj Kumar Pradhan <sarojkp.2k@gmail.com>
2021-03-07 20:43:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/exitalertdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.exitalertdemo;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+//*******This code is used for Exit alert message for a application *********************************************************
+    @Override
+    public void onBackPressed()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.app_name);
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setMessage("Do you want to exit this app ?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+//******************************************************************************************************************************
+    public void next(View view) {
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
+
+    }
+}
